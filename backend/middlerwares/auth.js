9fafe2616425659e512737a5473259a9020abf08
Yash Shah <yshah@argusoft.com>
2023-10-03 17:34:44 +0530

    user auth middleware added

--- backend/middlerwares/auth.js
+++ backend/middlerwares/auth.js
@@ -1,12 +1,17 @@
-const jwt = require("jsonwebtoken");
+const { verify } = require("../utils/jwt");
 
 const userAuth = (req, res, next) => {
   try {
     const [, token] = req.headers.authorization.split(" ");
 
-    const user = jwt.decode(token);
-    
+    const user = verify(token);
+
+    req.user = user;
+    next();
   } catch (error) {
-    next(error);
+    res.status(403);
+    next(new Error("auth failed..."));
   }
 };
+
+module.exports = { userAuth };
