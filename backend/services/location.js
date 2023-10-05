082df1217a5ee8de0447b0220087abd0b57c88a5
Yash Shah <yshah@argusoft.com>
2023-10-05 14:10:46 +0530

    using node-fetch instead of axios

--- backend/services/location.js
+++ backend/services/location.js
@@ -1,16 +1,18 @@
-const axios = require("axios");
+const fetch = require("node-fetch");
 
 const IPSTACK_API_KEY = process.env.IPSTACK_API_KEY;
 
 module.exports = {
   async getLocationFromIP(ip = "202.131.111.147") {
     try {
-      const result = await axios.get(
+      const result = await fetch(
         `http://api.ipstack.com/${ip}?access_key=${IPSTACK_API_KEY}`
       );
 
-      const city = result.data.city;
-      const country = result.data.country_name;
+      const data = await result.json();
+      const city = data.city;
+
+      const country = data.country_name;
       return { city, country };
     } catch (error) {
       throw error;
