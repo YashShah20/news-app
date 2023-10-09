5f96728b514a86e110301c4184357c93eb03831b
Yash Shah <yshah@argusoft.com>
2023-10-09 12:07:29 +0530

    vue router setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
 import store from "./stores/index";
+
+import { createRouter, createWebHashHistory } from "vue-router";
+import routes from "./router/index";
+
+const router = createRouter({
+  history: new createWebHashHistory(),
+  routes,
+});
 
 const app = createApp(App);
 
 app.use(store);
+app.use(router);
 
 app.mount("#app");
