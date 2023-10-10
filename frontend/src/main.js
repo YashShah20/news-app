3086b8505ca784494cbf29399e4f084b642d8d6d
Yash Shah <yshah@argusoft.com>
2023-10-10 10:51:23 +0530

    replaced material-design-icons-iconfont icons with mdi icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,13 +10,11 @@ const router = createRouter({
   routes,
 });
 
-import "material-design-icons-iconfont/dist/material-design-icons.css";
-
 import "vuetify/styles";
-import { aliases, md } from "vuetify/iconsets/md";
+import "@mdi/font/css/materialdesignicons.css";
 
 import { createVuetify } from "vuetify";
-// import { aliases, mdi } from "vuetify/iconsets/mdi";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
@@ -24,10 +22,10 @@ const vuetify = createVuetify({
   components,
   directives,
   icons: {
-    defaultSet: "md",
+    defaultSet: "mdi",
     aliases,
     sets: {
-      md,
+      mdi,
     },
   },
 });
